e05bf5d3c141438313241f8fef7600cb3e8baeb0
keyurvpatel200 <keyurvpatel200@gmail.com>
2025-12-23 20:03:01 +0530

    Fix: add image optimized package.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
   plugins: [react(), svgr({
     svgrOptions: { exportType: 'default', ref: true, svgo: false, titleProp: true },
     include: '**/assets/icons/**/*.svg',
-  })],
+  }),
+  viteImagemin({
+    gifsicle: { optimizationLevel: 7 },
+    optipng: { optimizationLevel: 7 },
+    mozjpeg: { quality: 70 },
+    webp: { quality: 70 }
+  })
+  ],
+
   server: {
     port: 3000,
   },
